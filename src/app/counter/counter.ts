360aa13f2a97b6c9a633e86e585f0dd3f7193cdb
Benjamin Krug <benjaminkrug@yahoo.com>
2026-04-24 15:17:57 -0400

    Use a computed signal value

--- src/app/counter/counter.ts
+++ src/app/counter/counter.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { DecimalPipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MuchMath } from '../much-math/much-math';
@@ -14,8 +14,20 @@ export class Counter {
   count: number = 0;
   step: number = 1;
   array1: number[] = [ ];
+  countB = signal<number>(1);
+  //As one might expect, computedSum only gets updated when "countB" changes, not "count".
+  //The most recent value for "count" is used whenever "countB" changes.
+  computedSum = computed(() => {
+    return this.count + this.countB();
+  });
   adjust() : void {
     this.array1.push(this.count);
     this.count += this.step;
+  }
+  getClasses() : string[] {
+    return ["turtle", "puppy"];
+  }
+  isExpanded() {
+    return true;
   }
 }
