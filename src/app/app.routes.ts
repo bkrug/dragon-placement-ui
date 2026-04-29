02f18e59edb1532eb1424e13b814553c98808429
Benjamin Krug <benjaminkrug@yahoo.com>
2026-04-29 16:19:41 -0400

    Add navigation links to the home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 import { Counter } from './counter/counter';
 import { PopoverDatatableDemo } from "./ability-table/ability-table";
 import { JobList } from './job-list/job-list';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
   {
     path: 'counter',
     component: Counter
