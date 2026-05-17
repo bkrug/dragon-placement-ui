9c9bbfd91e06af5f6d758b1e14a5261a6190d078
Benjamin Krug <benjaminkrug@yahoo.com>
2026-05-16 22:48:53 -0400

    I'm not sure what assertions I want to make in these local-field tests, but here is one that is at least able to construct the component.

--- src/app/local-form/local-checkbox.spec.ts
+++ src/app/local-form/local-checkbox.spec.ts
@@ -1,22 +1,32 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { inputBinding } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { LocalCheckbox } from './local-fields';
 
 describe('LocalCheckbox', () => {
-  let component: LocalCheckbox;
-  let fixture: ComponentFixture<LocalCheckbox>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [LocalCheckbox],
     }).compileComponents();
-
-    fixture = TestBed.createComponent(LocalCheckbox);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
   });
 
-  it('should create', () => {
+  it('should create', async () => {
+    const formGroup = new FormGroup({
+      givenName: new FormControl('Frank', [ Validators.required ]),
+    });
+
+    const fixture = TestBed.createComponent(LocalCheckbox, {
+      bindings: [
+        inputBinding('id', () => 'given-name'),
+        inputBinding('fieldName', () => 'givenName'),
+        inputBinding('label', () => 'Given Name:'),
+        inputBinding('formGroup', () => formGroup),
+        inputBinding('fieldControl', () => formGroup.get('givenName'))
+      ]
+    });
+    const component = fixture.componentInstance;
+    await fixture.whenStable();
+    
     expect(component).toBeTruthy();
   });
 });
