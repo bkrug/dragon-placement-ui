9d8cab0b2ab55a9761d1562f72e7b4a77f73e08b
Benjamin Krug <benjaminkrug@yahoo.com>
2026-05-18 16:46:24 -0400

    Make a test slightly more meaningful

--- src/app/much-math/much-math.spec.ts
+++ src/app/much-math/much-math.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { inputBinding } from '@angular/core';
 import { MuchMath } from './much-math';
 
 describe('MuchMath', () => {
@@ -10,13 +11,20 @@ describe('MuchMath', () => {
     await TestBed.configureTestingModule({
       imports: [MuchMath],
     }).compileComponents();
-
-    fixture = TestBed.createComponent(MuchMath);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
   });
 
-  it('should create', () => {
+  it('should create', async () => {
+    fixture = TestBed.createComponent(MuchMath, {
+      bindings: [
+        inputBinding('inputNumber', () => 1.0023)
+      ]
+    });
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+    const nativeElement = fixture.nativeElement as HTMLElement;
+
+    //Act
     expect(component).toBeTruthy();
+    expect(nativeElement.querySelector('p')?.textContent).toEqual('Given: 1.002 -- Double 2.0046 -- Reciprocal 0.998 -- Square: 1.00460529')
   });
 });
