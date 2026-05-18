da39e8f0f1f60182b26ff971901fe0de9b8077c7
Benjamin Krug <benjaminkrug@yahoo.com>
2026-05-18 16:14:58 -0400

    Make one more test pass uselessly

--- src/app/dragon-view/dragon-view.spec.ts
+++ src/app/dragon-view/dragon-view.spec.ts
@@ -1,5 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
+import { AssignmentHttpClient } from '../../httpClients/assignment-http-client';
+import { JobInclusions } from '../../poco/enums';
+import { Dragon } from '../../poco/models';
+import { ValidatedPayload } from '../../poco/standard-responses';
+import { MockActivatedRoute } from '../../testHelpers/MockActivatedRoute';
 import { DragonView } from './dragon-view';
 
 describe('DragonView', () => {
@@ -10,13 +15,51 @@ describe('DragonView', () => {
     await TestBed.configureTestingModule({
       imports: [DragonView],
     }).compileComponents();
+  });
 
+  it('should create', async () => {
+    class MockHttpClient implements AssignmentHttpClient {
+      getOnePageOfDragons = AssignmentHttpClient.prototype.getOnePageOfDragons;
+      getOnePageOfCandidates = AssignmentHttpClient.prototype.getOnePageOfCandidates;
+      getOnePageOfAssignees = AssignmentHttpClient.prototype.getOnePageOfAssignees;
+
+      async getDragonWithJobs(dragonId: number, jobInclusions: JobInclusions) {
+        return {
+          isInternalError: false,
+          isSuccess: true,
+          validationFailures: [],
+          payload: {
+            dragonId: dragonId,
+            givenName: 'Girbit',
+            familyName: 'Smokeson',
+            canBreathFire: true,
+            canTakePassengers: true,
+            weightInKg: null,
+            lengthInMeters: null,
+            fightingSkills: null,
+            assignments: []
+          } as Dragon
+        } as ValidatedPayload<Dragon>;
+      };
+
+      assignDragonToJob = AssignmentHttpClient.prototype.assignDragonToJob;
+      unassignDragonToJob = AssignmentHttpClient.prototype.unassignDragonToJob;
+      postDragonForm = AssignmentHttpClient.prototype.postDragonForm;
+      putDragonForm = AssignmentHttpClient.prototype.putDragonForm;
+    };      
+    const mockActivatedRoute = new MockActivatedRoute();
+    mockActivatedRoute.setParams({ dragonId: 15 });
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: AssignmentHttpClient, useClass: MockHttpClient },
+        { provide: ActivatedRoute, useValue: mockActivatedRoute }
+      ]
+    });
     fixture = TestBed.createComponent(DragonView);
     component = fixture.componentInstance;
     await fixture.whenStable();
-  });
 
-  it('should create', () => {
+    //Act
     expect(component).toBeTruthy();
   });
 });
