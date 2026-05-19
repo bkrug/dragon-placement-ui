f2d9f6a04946689b4feea4c4159a9edbf4cf8092
Benjamin Krug <benjaminkrug@yahoo.com>
2026-05-18 21:45:45 -0400

    Assert that values will be edited when the user clicks the Submit button.

--- src/app/pages/dragon-form/dragon-form.spec.ts
+++ src/app/pages/dragon-form/dragon-form.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { Effect } from 'effect';
 import { AssignmentHttpClient } from '../../../httpClients/assignment-http-client';
@@ -107,21 +108,16 @@ describe('Dragon Form Tests', () => {
         payload: JSON.parse(JSON.stringify(initialDbRecord))
       } as ValidatedPayload<Dragon>;
     };
+    let actualRecordIdInPutRequest: number = -1;
+    let actualModelInPutRequest: Dragon = new Dragon();
     mockHttpClient.putDragonForm = async (dragonId: number, dragon: Dragon) => {
+      actualRecordIdInPutRequest = dragonId;
+      actualModelInPutRequest = dragon;
       const validatedPayload = {
         isInternalError: false,
         isSuccess: true,
         validationFailures: [],
-        payload: {
-          dragonId: dragonId,
-          givenName: 'Girbit',
-          familyName: 'Smokeson',
-          canBreathFire: true,
-          canTakePassengers: false,
-          lengthInMeters: 35,
-          weightInKg: 2409,
-          fightingSkills: 'b'
-        } as Dragon
+        payload: JSON.parse(JSON.stringify(initialDbRecord))
       } as ValidatedPayload<Dragon>;
       return Effect.succeed(validatedPayload) as Effect.Effect<ValidatedPayload<Dragon>, ValidatedForm<DragonValidationFailures>, never>;
     };
@@ -142,10 +138,35 @@ describe('Dragon Form Tests', () => {
     component = fixture.componentInstance;
     await fixture.whenStable();
     
-    //Assert
+    //Assert values at load
     expect(component).toBeTruthy();
     const nativeElement: HTMLDivElement = fixture.nativeElement;
     const givenNameInput = nativeElement.querySelector('#given-name input') as HTMLInputElement;
-    expect(givenNameInput.value).toEqual('Girbit');
+    expect(givenNameInput.value).toEqual(initialDbRecord.givenName);
+    const familyNameInput = nativeElement.querySelector('#family-name input') as HTMLInputElement;
+    expect(familyNameInput.value).toEqual(initialDbRecord.familyName);
+    const canBreathFireCheck = nativeElement.querySelector('#can-breath-fire input') as HTMLInputElement;
+    expect(canBreathFireCheck.checked).toEqual(initialDbRecord.canBreathFire);
+    const canTakePassengersCheck = nativeElement.querySelector('#can-take-passengers input') as HTMLInputElement;
+    expect(canTakePassengersCheck.checked).toEqual(initialDbRecord.canTakePassengers);
+    const lengthInMetersInput = nativeElement.querySelector('#length-in-meters input') as HTMLInputElement;
+    expect(lengthInMetersInput.valueAsNumber).toEqual(initialDbRecord.lengthInMeters);
+    const weightInput = nativeElement.querySelector('#weight-in-kg input') as HTMLInputElement;
+    expect(weightInput.valueAsNumber).toEqual(initialDbRecord.weightInKg);
+    const fightingSkillsSelect = nativeElement.querySelector('#fighting-skills select') as HTMLSelectElement;
+    expect(fightingSkillsSelect.value).toEqual(initialDbRecord.fightingSkills);
+
+    //Act: change form values
+    component.dragonFormGroup().get('givenName')?.setValue('Gilbert');
+    component.dragonFormGroup().get('canTakePassengers')?.setValue(true);
+    const submitButton = fixture.debugElement.query(By.css('button'));
+    //submitButton.triggerEventHandler('click');
+    component.onSubmit();
+    await fixture.whenStable();
+
+    //Assert record changed
+    expect(actualRecordIdInPutRequest).toEqual(recordId);
+    expect(actualModelInPutRequest.givenName).toEqual('Gilbert');
+    expect(actualModelInPutRequest.canTakePassengers).toEqual(true);
   });
 });
