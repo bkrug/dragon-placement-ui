1f4d16880bd92279418bb312e6fd019274c93fbd
Benjamin Krug <benjaminkrug@yahoo.com>
2026-05-23 15:08:27 -0400

    Create a new dragon table component.

--- src/app/pages/manage-job/manage-job.ts
+++ src/app/pages/manage-job/manage-job.ts
@@ -1,15 +1,16 @@
 import { DatePipe } from '@angular/common';
-import { Component, inject, OnInit, signal, viewChildren } from '@angular/core';
+import { Component, inject, OnInit, signal, viewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AssignmentHttpClient } from '../../../httpClients/assignment-http-client';
 import { DragonTableType } from '../../../poco/enums';
 import { DisplayJob } from '../../../poco/models';
 import { mapJobToDisplayJob } from '../../../transformers';
+import { AssignedDragonTable } from '../../shared-components/assigned-dragon-table/assigned-dragon-table';
 import { DragonTable } from '../../shared-components/dragon-table/dragon-table';
 
 @Component({
   selector: 'app-manage-job',
-  imports: [DatePipe, DragonTable],
+  imports: [DatePipe, AssignedDragonTable, DragonTable],
   templateUrl: './manage-job.html',
   styleUrl: './manage-job.scss',
 })
@@ -17,7 +18,8 @@ export class ManageJob implements OnInit {
   private activatedRoute = inject(ActivatedRoute);
   private httpClient = inject(AssignmentHttpClient);
 
-  dragonTables = viewChildren(DragonTable);
+  assignedDragonTable = viewChild(AssignedDragonTable);
+  candidateTable = viewChild(DragonTable);
   selectedJob = signal<DisplayJob | null>(null);
   DragonTableType = DragonTableType;
 
@@ -27,12 +29,11 @@ export class ManageJob implements OnInit {
       this.httpClient.getJob(jobId).then(validatedPayload => {
         this.selectedJob.set(mapJobToDisplayJob(validatedPayload.payload));
       });
-    });    
+    });
   }
 
   reloadDragonTables() {
-    this.dragonTables().forEach(element => {
-      element.forcePageLoad();
-    });
+    this.assignedDragonTable()?.forcePageLoad();
+    this.candidateTable()?.forcePageLoad();
   }
 }
---
+++ src/app/shared-components/assigned-dragon-table/assigned-dragon-table.ts
@@ -0,0 +1,58 @@
+import { Component, EventEmitter, inject, input, OnInit, Output, signal } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { PAGE_SIZE } from '../../../global-consts';
+import { AssignmentHttpClient } from '../../../httpClients/assignment-http-client';
+import { DisplayJob, Dragon } from '../../../poco/models';
+import { ValidatedResponse } from '../../../poco/standard-responses';
+
+@Component({
+  selector: 'app-assigned-dragon-table',
+  imports: [TableModule, ButtonModule],
+  templateUrl: './assigned-dragon-table.html',
+  styleUrl: './assigned-dragon-table.scss',
+})
+export class AssignedDragonTable implements OnInit {
+  httpClient = inject(AssignmentHttpClient);
+
+  selectedJob = input<DisplayJob | null>();
+
+  @Output() unassignedDragon = new EventEmitter();
+
+  dragons = signal<Dragon[]>([]);
+  totalRecords = signal(0);
+  first = input(0);
+  readonly pageSize = PAGE_SIZE;
+
+  ngOnInit(): void {
+    this.forcePageLoad();
+  }
+
+  forcePageLoad() {
+    if (this.selectedJob() === null) return;
+    this.httpClient.getOnePageOfAssignees(this.selectedJob()!.jobId, this.first(), this.pageSize)
+      .then(pagedData => {
+        this.dragons.set(pagedData.data);
+        this.totalRecords.set(pagedData.totalRecords);
+      });
+  }
+
+  getSkillTagsDisplay(dragon: Dragon): string {
+    const skillString = dragon.skillTags.map(st => st.skillName).join(', ');
+    return skillString.length <= 50
+      ? skillString
+      : skillString.substring(0, 47) + '...';
+  }
+
+  unassignDragon(dragonId: number) {
+    if (this.selectedJob() === null) return;
+    this.httpClient.unassignDragonToJob(dragonId, this.selectedJob()!.jobId)
+      .then((validatedResponse: ValidatedResponse) => {
+        if (validatedResponse.isSuccess) {
+          this.unassignedDragon.emit();
+        } else {
+          alert(validatedResponse.validationFailures.join(', '));
+        }
+      });
+  }
+}
