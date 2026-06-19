e071e1e79e4e6221676f8b22b01f9bc2ba12b11b
Benjamin Krug <benjaminkrug@yahoo.com>
2026-06-18 23:54:48 -0400

    Add call to endpoint

--- src/httpClients/assignment-http-client.ts
+++ src/httpClients/assignment-http-client.ts
@@ -92,4 +92,8 @@ export class HoursWorkedClient {
   async getOnePageOfPayPeriods(dragonId: number, assignmentId: number, offset: number, limit: number) {
     return await HttpHelpers.getOnePage<PayPeriod>(`${apiUrl}dragon/${dragonId}/assignment/${assignmentId}/payperiod?offset=${offset}&limit=${limit}`);
   }
+
+  async getPayPeriodCandidates(dragonId: number, assignmentId: number) {
+    return await HttpHelpers.requestValidatedPayload<PayPeriod[]>(`${apiUrl}dragon/${dragonId}/assignment/${assignmentId}/payperiodcandidate`);
+  }  
 }
