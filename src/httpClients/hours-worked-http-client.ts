941ea8ebaf266e15c1f44b77efbbf0a997a19d37
Benjamin Krug <benjaminkrug@yahoo.com>
2026-06-20 13:09:46 -0400

    Delete obsolete Http Requests

--- src/httpClients/hours-worked-http-client.ts
+++ src/httpClients/hours-worked-http-client.ts
@@ -9,22 +9,6 @@ import { HttpHelpers } from './http-helpers';
   providedIn: 'root',
 })
 export class HoursWorkedClient {
-  async getOnePageOfHoursWorked(dragonId: number, assignmentId: number, offset: number, limit: number) {
-    return await HttpHelpers.getOnePage<HoursWorkedWithJob>(`${apiUrl}dragon/${dragonId}/assignment/${assignmentId}/hoursworked?offset=${offset}&limit=${limit}`);
-  }
-
-  async getHoursWorked(hoursWorkedId: number) {
-    return await HttpHelpers.requestValidatedPayload<HoursWorked>(`${apiUrl}hoursworked/${hoursWorkedId}`);
-  }
-
-  async postHoursWorkedForm(body: HoursWorkedCreateEdit) {
-    return await HttpHelpers.submitForm<HoursWorked, HoursWorkedValidationFailures>(`${apiUrl}hoursworked`, 'POST', body);
-  }
-
-  async putHoursWorkedForm(hoursWorkedId: number, body: HoursWorkedCreateEdit) {
-    return await HttpHelpers.submitForm<HoursWorked, HoursWorkedValidationFailures>(`${apiUrl}hoursworked/${hoursWorkedId}`, 'PUT', body);
-  }
-
   async getOnePageOfPayPeriods(dragonId: number, assignmentId: number, offset: number, limit: number) {
     return await HttpHelpers.getOnePage<PayPeriod>(`${apiUrl}dragon/${dragonId}/assignment/${assignmentId}/payperiod?offset=${offset}&limit=${limit}`);
   }
