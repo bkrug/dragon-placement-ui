4bac7ff0df548412817b4d2beaf3717710564c7e
Benjamin Krug <benjaminkrug@yahoo.com>
2026-06-19 21:30:54 -0400

    Populate the form more completely.

--- src/app/pages/pay-period-form/pay-period-form.ts
+++ src/app/pages/pay-period-form/pay-period-form.ts
@@ -2,14 +2,15 @@ import { DatePipe } from '@angular/common';
 import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { SelectModule } from 'primeng/select';
 import { HoursWorkedClient } from '../../../httpClients/assignment-http-client';
 import { getDateStringFromUnixSeconds } from '../../../misc/transformers';
 import { PayPeriod } from '../../../poco/models';
-import { LocalSelectField, SelectListOption } from '../../local-form/local-fields';
+import { SelectListOption } from '../../local-form/local-fields';
 
 @Component({
   selector: 'app-pay-period-form',
-  imports: [ReactiveFormsModule, LocalSelectField, DatePipe],
+  imports: [ReactiveFormsModule, SelectModule, DatePipe],
   templateUrl: './pay-period-form.html',
   styleUrl: './pay-period-form.scss',
 })
@@ -28,7 +29,10 @@ export class PayPeriodForm implements OnInit {
   );
 
   formGroup = signal(new FormGroup({
-    startDateUnix: new FormControl<string | null>(null, [Validators.required]),
+    dragonId: new FormControl<number>(0),
+    assignmentId: new FormControl<number>(0),
+    startDateUnix: new FormControl<number | null>(null, [Validators.required]),
+    endDateUnix: new FormControl<number | null>(null, [Validators.required]),
   }));
 
   selectedCandidate = signal<PayPeriod | null>(null);
@@ -37,16 +41,26 @@ export class PayPeriodForm implements OnInit {
     this.route.params.subscribe(params => {
       this.dragonId = params['dragonId'] || 0;
       this.assignmentId = params['assignmentId'] || 0;
+      this.formGroup().patchValue({
+        dragonId: this.dragonId,
+        assignmentId: this.assignmentId,
+      });
     });
   }
 
   ngOnInit() {
     this.httpClient.getPayPeriodCandidates(this.dragonId, this.assignmentId)
       .then(r => this.candidates.set(r.payload));
+  }
 
-    this.formGroup().get('startDateUnix')!.valueChanges.subscribe(value => {
-      const match = this.candidates().find(c => c.startDateUnix.toString() === value);
-      this.selectedCandidate.set(match ?? null);
-    });
+  onCandidateSelect(event: { value: string }) {
+    const match = this.candidates().find(c => c.startDateUnix.toString() === event.value);
+    if (match) {
+      this.selectedCandidate.set(match);
+      this.formGroup().patchValue({
+        startDateUnix: match.startDateUnix,
+        endDateUnix: match.endDateUnix,
+      });
+    }
   }
 }
