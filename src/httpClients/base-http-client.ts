72e5968c8b927505252cd202e79a244476528ad3
Benjamin Krug <benjaminkrug@yahoo.com>
2026-08-19 14:21:16 -0400

    Refactor whitespace

--- src/httpClients/base-http-client.ts
+++ src/httpClients/base-http-client.ts
@@ -16,57 +16,66 @@ export class BaseHttpClient {
   }
 
   protected getOnePage<T extends object>(url: string): Observable<PagedData<T>> {
-    return this.register(fromFetch(url).pipe(
-      switchMap(response => response.json() as Promise<PagedData<T>>)
-    ));
+    return this
+      .register(fromFetch(url)
+      .pipe(
+        switchMap(response => response.json() as Promise<PagedData<T>>)
+      ));
   }
 
   protected requestValidatedResponse(url: string, httpVerb: 'GET' | 'POST' | 'PUT' | 'DELETE')
       : Observable<ValidatedResponse> {
-    return this.register(fromFetch(url, { method: httpVerb }).pipe(
-      switchMap(response => response.json() as Promise<ValidatedResponse>),
-      catchError(ex => of({
-        isSuccess: false,
-        isInternalError: true,
-        validationFailures: [JSON.stringify(ex)]
-      } as ValidatedResponse))
-    ));
+    return this
+      .register(fromFetch(url, { method: httpVerb })
+      .pipe(
+        switchMap(response => response.json() as Promise<ValidatedResponse>),
+        catchError(ex => of({
+          isSuccess: false,
+          isInternalError: true,
+          validationFailures: [JSON.stringify(ex)]
+        } as ValidatedResponse))
+      ));
   }
 
   protected requestValidatedPayload<T extends object>(url: string, httpVerb: 'GET' | 'POST' | 'PUT' | 'DELETE' = 'GET')
       : Observable<ValidatedPayload<T>> {
-    return this.register(fromFetch(url, { method: httpVerb }).pipe(
-      switchMap(response => response.json() as Promise<ValidatedPayload<T>>),
-      catchError(ex => of({
-        isSuccess: false,
-        isInternalError: true,
-        validationFailures: [JSON.stringify(ex)]
-      } as ValidatedPayload<T>))
-    ));
+    return this
+      .register(fromFetch(url, { method: httpVerb })
+      .pipe(
+        switchMap(response => response.json() as Promise<ValidatedPayload<T>>),
+        catchError(ex => of({
+          isSuccess: false,
+          isInternalError: true,
+          validationFailures: [JSON.stringify(ex)]
+        } as ValidatedPayload<T>))
+      ));
   }
 
   protected submitForm<Tok extends object, Tfail extends object>(url: string, httpVerb: 'PUT' | 'POST', requestBody: object)
       : Observable<Effect.Effect<ValidatedPayload<Tok>, ValidatedForm<Tfail>, never>> {
     const headers = new Headers();
     headers.set('Content-Type', 'application/json');
-    return this.register(fromFetch(url, {
+    const requestInit = {
       method: httpVerb,
       headers: headers,
       body: JSON.stringify(requestBody)
-    }).pipe(
-      switchMap(async response => {
-        const json = await response.json();
-        return response.ok
-          ? Effect.succeed(json as ValidatedPayload<Tok>)
-          : Effect.fail(json as ValidatedForm<Tfail>);
-      }),
-      catchError(ex => {
-        console.error(JSON.stringify(ex));
-        return of(Effect.fail({
-          isSuccess: false,
-          isInternalError: true
-        } as ValidatedForm<Tfail>) as Effect.Effect<ValidatedPayload<Tok>, ValidatedForm<Tfail>, never>);
-      })
-    ));
+    };
+    return this
+      .register(fromFetch(url, requestInit)
+      .pipe(
+        switchMap(async response => {
+          const json = await response.json();
+          return response.ok
+            ? Effect.succeed(json as ValidatedPayload<Tok>)
+            : Effect.fail(json as ValidatedForm<Tfail>);
+        }),
+        catchError(ex => {
+          console.error(JSON.stringify(ex));
+          return of(Effect.fail({
+            isSuccess: false,
+            isInternalError: true
+          } as ValidatedForm<Tfail>) as Effect.Effect<ValidatedPayload<Tok>, ValidatedForm<Tfail>, never>);
+        })
+      ));
   }
 }
